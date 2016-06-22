6d98bdea691a6b911c12bfc70db9a25657f53675
Nikita.Leshchev <nikita@amethyst-ws.ru>
2016-06-22 11:08:58 +0300

    Практически готово

--- js/script.js
+++ js/script.js
@@ -4,38 +4,41 @@
 $(window).load(function(){
     setTimeout(function(){
         $('.cd-transition-layer').addClass('opening');
+        setTimeout(function(){
+            $('.bottom_links, .head_links').addClass('visible');
+        }, 1100);
     }, 100);
-    var frameProportion = 1.77777, //png frame aspect ratio
-        frames = 10, //number of png frames
-        resize = false;
+});
+var frameProportion = 1.77777, //png frame aspect ratio
+    frames = 10, //number of png frames
+    resize = false;
 
 //set transitionBackground dimentions
-    setLayerDimensions();
-    $(window).on('resize', function(){
-        if( !resize ) {
-            resize = true;
-            (!window.requestAnimationFrame) ? setLayerDimensions() : window.requestAnimationFrame(setLayerDimensions);
-        }
-    });
-
-    function setLayerDimensions() {
-        var windowWidth = $(window).width(),
-            windowHeight = $(window).height(),
-            layerHeight, layerWidth;
-
-        if( windowWidth/windowHeight > frameProportion ) {
-            layerWidth = windowWidth;
-            layerHeight = layerWidth/frameProportion;
-        } else {
-            layerHeight = windowHeight;
-            layerWidth = layerHeight*frameProportion;
-        }
-
-        $('.bg-layer').css({
-            'width': layerWidth*frames+'px',
-            'height': layerHeight+'px',
-        });
-
-        resize = false;
+setLayerDimensions();
+$(window).on('resize', function(){
+    if( !resize ) {
+        resize = true;
+        (!window.requestAnimationFrame) ? setLayerDimensions() : window.requestAnimationFrame(setLayerDimensions);
     }
 });
+
+function setLayerDimensions() {
+    var windowWidth = $(window).width(),
+        windowHeight = $(window).height(),
+        layerHeight, layerWidth;
+
+    if( windowWidth/windowHeight > frameProportion ) {
+        layerWidth = windowWidth;
+        layerHeight = layerWidth/frameProportion;
+    } else {
+        layerHeight = windowHeight;
+        layerWidth = layerHeight*frameProportion;
+    }
+
+    $('.bg-layer').css({
+        'width': layerWidth*frames+'px',
+        'height': layerHeight+'px',
+    });
+
+    resize = false;
+}
